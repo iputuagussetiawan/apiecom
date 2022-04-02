71a0bfceded647a5abf91ef85296a625527f7fea
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-02 12:22:34 +0800

    314. Select Category Subcategory And Make JSON Array

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,12 +5,24 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Subcategory;
 
 class CategoryController extends Controller
 {
     public function AllCategory()
     {
         $categories = Category::all();
-        return $categories;
+        $categoryDetailArray = [];
+
+        foreach ($categories as $value) {
+            $subcategory = Subcategory::where('category_name', $value['category_name'])->get();
+            $item = [
+                'category_name' => $value['category_name'],
+                'category_image' => $value['category_image'],
+                'subcategory_name' =>  $subcategory
+            ];
+            array_push($categoryDetailArray, $item);
+        }
+        return $categoryDetailArray;
     }
 }
