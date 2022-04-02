0cc6c6e045730616ecac23eaf9539f7d5bce5612
I PUTU AGUS SETIAWAN <agussetiawaniputu@gmail.com>
2022-04-02 11:52:40 +0800

    313. Create Category and Subcategory Table

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
+use App\Http\Controllers\Admin\CategoryController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -18,3 +19,5 @@ Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
 Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
 //Site Info
 Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteinfo']);
+//Category Controller
+Route::get('/allcategory', [CategoryController::class, 'AllCategory']);
